f8488a2fb0a1ed880c871d0deb44b28a35a714d3
Bawazier <32612909+Bawazier@users.noreply.github.com>
2020-12-01 22:48:22 +0700

    fix sequelize models

--- src/routes/worker.js
+++ src/routes/worker.js
@@ -1,6 +1,7 @@
 const router = require('express').Router()
 const account = require('../controllers/Worker/account')
 const workExperience = require('../controllers/Worker/workExperience')
+const portofolio = require('../controllers/Worker/portofolio')
 
 router.get('/account', account.getAccount)
 router.patch('/account', account.patchAccount)
@@ -12,5 +13,12 @@ router.put('/experience/:id', workExperience.putWorkExperience)
 router.get('/experience/:id', workExperience.getWorkExperience)
 router.get('/experience', workExperience.listWorkExperience)
 router.delete('/experience/:id', workExperience.deleteWorkExperience)
+
+router.post('/portofolio', portofolio.postPortofolio)
+router.patch('/portofolio/:id', portofolio.patchPortofolio)
+router.put('/portofolio/:id', portofolio.putPortofolio)
+router.get('/portofolio/:id', portofolio.getPortofolio)
+router.get('/portofolio', portofolio.listPortofolio)
+router.delete('/portofolio/:id', portofolio.deletePortofolio)
 
 module.exports = router
