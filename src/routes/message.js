bf075bcbaa8decce27b4bdefda88f785e85698d5
M Faris Alkodri <farisalkodri@gmail.com>
2020-12-03 13:25:40 +0700

    edit issues on send message

--- src/routes/message.js
+++ src/routes/message.js
@@ -4,6 +4,7 @@ const messager = require('../controllers/Message/message')
 route.post('/chat/:id', messager.sendMessage)
 route.get('/chat/:id', messager.getMessage)
 route.patch('/chat/:id', messager.updateMessage)
+route.patch('/read/:id', messager.updateRead)
 route.delete('/chat/:id', messager.deleteMessage)
 
 route.get('/list/person', messager.listPerson)
